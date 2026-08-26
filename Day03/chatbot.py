1c7b5b20a6454ff55dba15e585a825867307f018
CyberHunter8857 <mayurtamanke7744@gmail.com>
2026-08-26 11:32:08 +0530

    Day 3: Built my first LLM-powered AI chatbot

--- Day03/chatbot.py
+++ Day03/chatbot.py
@@ -6,9 +6,16 @@ load_dotenv()
 
 client =  genai.Client(api_key= os.getenv("GOOGLE_API_KEY"))
 
-response= client.models.generate_content(
-    model="gemini-3.5-flash",
-    contents="Explain Agentic AI in simple words."
-)
+print("Gemini ChatBot( type exit to quit )")
 
-print(response.text)
+while True:
+    user= input("You: ")
+    if user.lower() == "exit":
+        break
+    res= client.models.generate_content (
+        model= "gemini-3.5-flash",
+        contents= user
+    )
+
+    print("AI: ", res.text)
+    print("_"*30)
